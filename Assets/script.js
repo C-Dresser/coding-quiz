9bd7d9254c769feb18b2240233abca9342852498
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2023-11-10 19:56:16 -0500

    added quiz and end sections to html and starting scripting a way to manage screen visibility in js

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,34 @@
-console.log("Hello");
+//Housekeeping
+var startScreen = document.querySelector('.start');
+var question1Screen = document.querySelector('.q1');
+var endScreen = document.querySelector('.end');
+var startButton = document.querySelector('#start-btn')
+//Added show screen functions
+function showStart() {
+    startScreen.style.display = null;
+    question1Screen.style.display = "none";
+    endScreen.style.display = "none";
+}
+
+function showQ1() {
+    startScreen.style.display = "none";
+    question1Screen.style.display = null;
+    endScreen.style.display = "none";
+}
+
+function showEnd() {
+    startScreen.style.display = "none";
+    question1Screen.style.display = "none";
+    endScreen.style.display = null;
+}
+//added functionality to start button
+startButton.addEventListener('click', function(event) {
+    showQ1();
+    console.log('clicked')
+});
+//Made start the default screen
+function init () {
+    showStart();
+}
+
+init();
