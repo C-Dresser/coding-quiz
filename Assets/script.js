70c80115a4ff5926298ceeb80348085b82f03552
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2023-11-11 20:21:59 -0500

    added additonal questoins to quiz and changed html and javsscript accordinly

--- Assets/script.js
+++ Assets/script.js
@@ -1,33 +1,87 @@
 //Housekeeping
 var startScreen = document.querySelector('.start');
 var question1Screen = document.querySelector('.q1');
+var question2Screen = document.querySelector('.q2');
+var question3Screen = document.querySelector('.q3');
+var question4Screen = document.querySelector('.q4');
+var question5Screen = document.querySelector('.q5');
 var endScreen = document.querySelector('.end');
 var startButton = document.querySelector('#start-btn');
-//var correct = document.querySelector('[data-type="correct"]');
-//var incorrect = document.querySelector('[data-type="incorrect"]');
-//var answerButton = document.querySelector('.answer')
 
 //Added show screen functions
+//Added show functions for new questions
 function showStart() {
     startScreen.style.display = null;
     question1Screen.style.display = "none";
+    question2Screen.style.display = "none";
+    question3Screen.style.display = "none";
+    question4Screen.style.display = "none";
+    question5Screen.style.display = "none";
     endScreen.style.display = "none";
 }
 
 function showQ1() {
     startScreen.style.display = "none";
     question1Screen.style.display = null;
+    question2Screen.style.display = "none";
+    question3Screen.style.display = "none";
+    question4Screen.style.display = "none";
+    question5Screen.style.display = "none";
+    endScreen.style.display = "none";
+}
+
+function showQ2() {
+    startScreen.style.display = "none";
+    question1Screen.style.display = "none";
+    question2Screen.style.display = null;;
+    question3Screen.style.display = "none";
+    question4Screen.style.display = "none";
+    question5Screen.style.display = "none";
+    endScreen.style.display = "none";
+}
+
+function showQ3() {
+    startScreen.style.display = "none";
+    question1Screen.style.display = "none";
+    question2Screen.style.display = "none";
+    question3Screen.style.display = null;;
+    question4Screen.style.display = "none";
+    question5Screen.style.display = "none";
+    endScreen.style.display = "none";
+}
+
+function showQ4() {
+    startScreen.style.display = "none";
+    question1Screen.style.display = "none";
+    question2Screen.style.display = "none";
+    question3Screen.style.display = "none";
+    question4Screen.style.display = null;;
+    question5Screen.style.display = "none";
+    endScreen.style.display = "none";
+}
+
+function showQ5() {
+    startScreen.style.display = "none";
+    question1Screen.style.display = "none";
+    question2Screen.style.display = "none";
+    question3Screen.style.display = "none";
+    question4Screen.style.display = "none";
+    question5Screen.style.display = null;;
     endScreen.style.display = "none";
 }
 
 function showEnd() {
     startScreen.style.display = "none";
     question1Screen.style.display = "none";
+    question2Screen.style.display = "none";
+    question3Screen.style.display = "none";
+    question4Screen.style.display = "none";
+    question5Screen.style.display = "none";
     endScreen.style.display = null;
 }
 //THIS FUNCTION DOES NOT WORK PROPERLY!!! FIX AFTER WORK TOMORROW!
 //Fixed function, it now checks if an answer is correct or not and logs it to the console. 
-function checkAnswer() {
+function checkAnswer(event) {
     var answer = event.target;
     var state = answer.getAttribute("data-type");
 
@@ -43,14 +97,42 @@ startButton.addEventListener('click', function(event) {
     showQ1();
 });
 //added functionality to answer buttons
-question1Screen.addEventListener('click', function(event){
+//added eventListeners to new questions
+question1Screen.addEventListener('click', function (event) {
     if (event.target.matches('button')) {
-        checkAnswer();
-        showEnd();
-    } else {
-
+        checkAnswer(event);
+        showQ2();
     }
 });
+
+question2Screen.addEventListener('click', function (event) {
+    if (event.target.matches('button')) {
+        checkAnswer(event);
+        showQ3();
+    }
+});
+
+question3Screen.addEventListener('click', function (event) {
+    if (event.target.matches('button')) {
+        checkAnswer(event);
+        showQ4();
+    }
+});
+
+question4Screen.addEventListener('click', function (event) {
+    if (event.target.matches('button')) {
+        checkAnswer(event);
+        showQ5();
+    }
+});
+
+question5Screen.addEventListener('click', function (event) {
+    if (event.target.matches('button')) {
+        checkAnswer(event);
+        showEnd();
+    }
+});
+
 //Made start the default screen
 function init () {
     showStart();
